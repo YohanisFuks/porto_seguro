80c8f97a67635a0c4419ba083d2704290b0bace7
YohanisFuks <fuks.yohanis@gmail.com>
2023-07-17 08:32:17 -0300

    setup inicial do lgbm setup inicial do DAE com comentario de codigo

---
+++ utils/eval_gini.py
@@ -0,0 +1,22 @@
+from numba import jit
+@jit
+def eval_gini(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_pred)]
+    ntrue = 0
+    gini = 0
+    delta = 0
+    n = len(y_true)
+    for i in range(n-1, -1, -1):
+        y_i = y_true[i]
+        ntrue += y_i
+        gini += y_i * delta
+        delta += 1 - y_i
+    gini = 1 - 2 * gini / (ntrue * (n - ntrue))
+    return gini
+def gini_lgb(y_true, y_pred):
+    eval_name = 'normalized_gini_coef'
+    eval_result = eval_gini(y_true, y_pred)
+    is_higher_better = True
+    return eval_name, eval_result, is_higher_better
+
